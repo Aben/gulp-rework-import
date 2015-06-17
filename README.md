9b28a8f95a6460928283784a182b17294f6a081f
Aben <tntaben@gmail.com>
2015-06-17 19:39:55 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# gulp-rework-import
+
+A gulp plugin for import several CSS files into a single file, Import each CSS file only once.
+
+Base on [rework-import](https://github.com/Aben/rework-import)
+
+## Usage
+
+```js
+gulp( SRC ).pipe( require('gulp-rework-import') )
+```
+
+## Install
+
+```js
+npm install gulp-rework-import
+```
+
---
+++ index.js
@@ -0,0 +1,26 @@
+var gutil = require('gulp-util'),
+var through = require('through2');
+var cssImprt = require('rework-import');
+var rework = require('rework');
+
+// Consts
+var PLUGIN_NAME = 'gulp-rework-import';
+
+module.exports = function() {
+  return through.obj(function (file, enc, next) {
+      if (file.isStream()) {
+          //todo support stream 
+          this.emit('error', new gutil.PluginError(PLUGIN_NAME, 'Streaming not supported'));
+          return next();
+      }
+
+      if (file.isBuffer()) {
+          var css = rework(file.contents.toString(enc)).use(cssImprt({ path: path.dirname(file.path) }))
+          file.contents = new Buffer(css.toString(enc))
+      }
+      // make sure the file goes through the next gulp plugin
+      this.push(file);
+      // tell the stream engine that we are done with this file
+      next();
+  })
+}
